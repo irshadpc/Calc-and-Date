32bb4ae07c2e8992d8ebef50c3dc0af9e0088ccd
Ishida Junichi <ishida@Ishida-no-MacBook-Air.local>
2012-12-15 18:33:03 +0900

    [-][*][/]のテストケースを追加

--- CalendarCalcTests/CCCalendarCalcTests.m
+++ CalendarCalcTests/CCCalendarCalcTests.m
@@ -32,34 +32,61 @@
     STAssertNotNil(_calendarCalc, @"CalendarCalc is nil");
 }
 
-- (void)testInputNumber1
+- (void)testInputNumber_1
 {
     NSString *const result = [[self inputString: @"1"] displayResult];
     STAssertEqualObjects(@"1", result, @"RESULT: %@", result);
 }
 
-- (void)testInputNumber12
+- (void)testInputNumber_12
 {
     [self inputString: @"1"];
     NSString *const result = [[self inputString: @"2"] displayResult];
     STAssertEqualObjects(@"12", result, @"RESULT: %@", result);
 }
 
-- (void)testInputDate20121215
-{
-    NSString *const result = [[_calendarCalc inputDate:[NSDate dateWithYear: 2012
-                                                                      month: 12
-                                                                        day: 15]] displayResult];
-    STAssertEqualObjects(@"2012/12/15", result, @"RESULT: %@", result);
-}
-
-- (void)testInputFunction1Plus2
+- (void)testInputFunction_1_Plus_2_Equal
 {
     [self inputString: @"1"];
     [_calendarCalc inputFunction: CCPlus];
     [self inputString: @"2"];
     NSString *const result = [[_calendarCalc inputFunction: CCEqual] displayResult];
     STAssertEqualObjects(@"3", result, @"RESULT: %@", result);
+}
+
+- (void)testInputFunction_2_Minus_1_Equal
+{
+    [self inputString: @"2"];
+    [_calendarCalc inputFunction: CCMinus];
+    [self inputString: @"1"];
+    NSString *const result = [[_calendarCalc inputFunction: CCEqual] displayResult];
+    STAssertEqualObjects(@"1", result, @"RESULT: %@", result);
+}
+
+- (void)testInputFunction_2_Multiply_3_Equal
+{
+    [self inputString: @"2"];
+    [_calendarCalc inputFunction: CCMultiply];
+    [self inputString: @"3"];
+    NSString *const result = [[_calendarCalc inputFunction: CCEqual] displayResult];
+    STAssertEqualObjects(@"6", result, @"RESULT: %@", result);
+}
+
+- (void)testInputFunction_6_Divide_2_Equal
+{
+    [self inputString: @"6"];
+    [_calendarCalc inputFunction: CCDivide];
+    [self inputString: @"2"];
+    NSString *const result = [[_calendarCalc inputFunction: CCEqual] displayResult];
+    STAssertEqualObjects(@"3", result, @"RESULT: %@", result);
+}
+
+- (void)testInputDate_2012_12_15
+{
+    NSString *const result = [[_calendarCalc inputDate:[NSDate dateWithYear: 2012
+                                                                      month: 12
+                                                                        day: 15]] displayResult];
+    STAssertEqualObjects(@"2012/12/15", result, @"RESULT: %@", result);
 }
 
 
