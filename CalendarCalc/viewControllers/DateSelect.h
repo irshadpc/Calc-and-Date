3aeebbb93abc43e751584affcc7fe3de7839712d
Ishida Junichi <dev.j.ishida@gmail.com>
2013-04-20 16:53:48 +0900

    ディレクトリ構成を変更

---
+++ CalendarCalc/viewControllers/DateSelect.h
@@ -0,0 +1,15 @@
+//
+//  CCDateSelect.h
+//  CalendarCalc
+//
+//  Created by Ishida Junichi on 2012/12/23.
+//  Copyright (c) 2012年 Ishida Junichi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Week.h"
+
+@protocol DateSelect<NSObject>
+- (void)didSelectDate:(NSDate *)date;
+- (void)didSelectWeek:(Week)week exclude:(BOOL)exclude;
+@end
---
+++ CalendarCalc/viewControllers/SettingViewController.h
@@ -0,0 +1,19 @@
+//
+//  CCSettingViewController.h
+//  CalendarCalc
+//
+//  Created by Ishida Junichi on 2012/12/31.
+//  Copyright (c) 2012年 Ishida Junichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SettingViewControllerDelegate;
+
+@interface SettingViewController : UIViewController
+@property (weak, nonatomic) id <SettingViewControllerDelegate> delegate;
+@end
+
+@protocol SettingViewControllerDelegate <NSObject>
+- (void)settingViewControllerDidFinish:(SettingViewController *)viewController;
+@end
